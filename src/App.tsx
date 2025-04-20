bd55452d3b5f0a9a9331b3896fe2c304b3df5d18
wordixai <wordixai@gmail.com>
2025-04-20 13:23:42 +0800

    Update files

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,7 @@ import NotFound from "./pages/NotFound";
 const queryClient = new QueryClient();
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-  </QueryClientProvider>
+  <Index />
 );
 
 export default App;
